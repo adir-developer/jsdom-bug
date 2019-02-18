c95d83cfa24096c3d29fa7506287fcc3768b752e
Adir Nahum <adir.n@walkme.com>
2019-02-18 10:14:50 +0200

    clean code

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 
-const { document } = (new JSDOM('')).window;
-const parent = document.createElement('div');
-parent.innerHTML =
+const html =
 	'<div class="first" style="font-size:15px">some text</div>' +
 	'<div class="second" style="font-size:1r5px">some text</div>';
-const first = parent.querySelector('div.first');
-const second = parent.querySelector('div.second');
+
+const { document } = (new JSDOM(html)).window;
+const first = document.querySelector('div.first');
+const second = document.querySelector('div.second');
 
 console.log(`first.style.length should be: 1, actual: ${first.style.length}`);
 console.log(`second.style.length should be: 0, actual: ${second.style.length}`);
